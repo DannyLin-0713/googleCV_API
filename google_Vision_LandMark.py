e1dd78e8660d5a79da1d631f957c3802e6780731
DannyLin-0713 <61904030+DannyLin-0713@users.noreply.github.com>
2020-09-17 08:22:22 +0400

    Add files via upload

---
+++ google_Vision_LandMark.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import requests
+import json
+import base64
+
+while True:
+    AuthoID=input("\033[1;33;40m Please input your autho ID \033[0m \n")
+    img_name=input("\033[1;33;40m Please input your img file name \033[0m  \n")
+    img_b = str(base64.b64encode(open(img_name,"rb").read()),'utf8')
+
+    url = 'https://vision.googleapis.com/v1/images:annotate?key=' + AuthoID
+    headers = {'content-type': 'application/json'}
+    requestData={
+          "requests": [
+            {
+              "image": {
+                "content": img_b
+              },
+              "features": [
+                {
+                  "maxResults": 3,
+                  "type": "LANDMARK_DETECTION"
+                }
+              ]
+            }
+          ]
+        }
+    after_call = requests.post(url, json=requestData, headers=headers)
+    text=json.loads(after_call.text)
+    paragum=int(0)
+    paragum_a=len(text['responses'][0]['landmarkAnnotations'])
+    print ("\033[1;32;40m The top3 possible WORLD-LAND-MARK of this picture is \033[0m")
+    
+    while paragum < paragum_a:
+        if after_call.status_code == 200:
+            print(text['responses'][0]['landmarkAnnotations'][paragum]['description'])
+            paragum = paragum + 1
+        else:
+            print("error, pls check the file or your autho id")
+            print (text)
